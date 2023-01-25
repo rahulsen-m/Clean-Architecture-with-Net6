7456d6f5bfab7d579476c2fa40ed928217e27026
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-01-25 16:31:15 +0530

    Implemented interfaces - added class for registering the persistence project

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,20 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepositories<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matchs = _dbContext.Events.Any(e => e.Name.Equals(name) &&
+                e.Date.Date.Equals(eventDate.Date));
+            return Task.FromResult(matchs);
+        }
+    }
+}
